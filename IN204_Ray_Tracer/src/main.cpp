c9ba92c7c78b4966e7ea0be3ac835023126990f2
Mattéo Sutour <matteo.sutour@gmail.com>
2021-03-08 15:00:25 +0100

    Rendu d'une scène de Sphere

--- IN204_Ray_Tracer/src/main.cpp
+++ IN204_Ray_Tracer/src/main.cpp
@@ -18,18 +18,19 @@ int main(int argc, const char** argv) {
     std::vector<std::unique_ptr<Object>> objects;
     // position, radius, surface color, reflectivity, transparency, emission color
     objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.2f, 0.2f, 0.2f), 0.5, 0.0)));
-    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00f, 0.32f, 0.36f), 1, 0.5)));
-    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00f, 0.32f, 0.36f), 1, 0.5)));
-    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90f, 0.76f, 0.46f), 1, 0.0)));
-    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65f, 0.77f, 0.97f), 1, 0.0)));
-    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90f, 0.90f, 0.90f), 1, 0.0)));
-    objects.push_back(std::unique_ptr<Object>(new Cube(Vec3f(-5.5, 0, -15), Vec3f(0, 5.5, -20.5), Vec3f(0.90f, 0.90f, 0.90f), 1, 0.0)));
+    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(-10, 4, -20), 4, Vec3f(1.00f, 0.20f, 0.20f), 1, 0.5)));
+    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(0.20f, 0.80f, 0.80f), 1, 0.5)));
+    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(8.0, -1, -15), 2, Vec3f(0.10f, 0.90f, 0.46f), 1, 0.5)));
+    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(6.0, 0, -25), 3, Vec3f(0.65f, 0.77f, 0.97f), 1, 0.5)));
+    objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.6f, 0.6f, 0.0f), 1, 0.5)));
+    //objects.push_back(std::unique_ptr<Object>(new Cube(Vec3f(-5.5, 0, -15), Vec3f(0, 5.5, -20.5), Vec3f(0.90f, 0.90f, 0.90f), 1, 0.0)));
     // light
     // Le dernier argument correspond à emissionColor, si emissionColor > 0 alors il s'agit d'une lumière.
     objects.push_back(std::unique_ptr<Object>(new Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3))));
     render(objects);
 
 
+    /*
     sf::ContextSettings settings;
     settings.depthBits = 24;
     settings.stencilBits = 8;
@@ -44,7 +45,7 @@ int main(int argc, const char** argv) {
     GLenum err = glewInit();
     if (GLEW_OK != err)
     {
-        /* Problem: glewInit failed, something is seriously wrong. */
+        // Problem: glewInit failed, something is seriously wrong.
         fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
     }
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
@@ -53,88 +54,6 @@ int main(int argc, const char** argv) {
 
     float pos[3] = { 0.0f, 0.0f, 0.0f };
     CubeShader MyCube(0.5f, pos);
-
-    /*
-    // Code pour le Vertex Shader (Position)
-    const char* vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-
-    // Code pour le Fragment Shader (Couleur)
-    const char* fragmentShaderSource = "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}\0";
-
-    //Sommets
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-    };
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    //Shader Program : fait le lien entre les shaders combin�s
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-    //On n'a plus besoin des vertex et fragment shaders li�s dans le shader program :
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    //On doit sp�cifier comment OpenGL interprete les donn�es de sommets :
-    glBindVertexArray(VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    */
 
     MyCube.renderCubeShader(0.5f, pos);
 
@@ -162,8 +81,8 @@ int main(int argc, const char** argv) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // dessin...
-        /*glUseProgram(shaderProgram);
-        glBindVertexArray(VBO);*/
+        //glUseProgram(shaderProgram);
+        //glBindVertexArray(VBO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
@@ -172,6 +91,7 @@ int main(int argc, const char** argv) {
     }
 
     // lib�ration des ressources...
+    */
     
     return 0;
 }
